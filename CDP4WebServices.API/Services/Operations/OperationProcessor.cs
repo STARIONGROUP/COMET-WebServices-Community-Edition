06862a01edb950c93cd9fb65dc2ee34bdd125a2b
lxatrhea <a.vandelft@rheagroup.com>
2020-06-16 15:24:12 +0200

    Delete File fix

--- CDP4WebServices.API/Services/Operations/OperationProcessor.cs
+++ CDP4WebServices.API/Services/Operations/OperationProcessor.cs
@@ -1,6 +1,26 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="OperationProcessor.cs" company="RHEA System S.A.">
-//   Copyright (c) 2015-2018 RHEA System S.A.
+//    Copyright (c) 2015-2020 RHEA System S.A.
+//
+//    Author: Sam Gerené, Merlin Bieze, Alex Vorobiev, Naron Phou, Alexander van Delft, Kamil Wojnowski, 
+//            Nathanael Smiechowski
+//
+//    This file is part of CDP4 Web Services Community Edition. 
+//    The CDP4 Web Services Community Edition is the RHEA implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+//    This is an auto-generated class. Any manual changes to this file will be overwritten!
+//
+//    The CDP4 Web Services Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    The CDP4 Web Services Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -1313,12 +1333,14 @@ namespace CDP4WebServices.API.Services.Operations
             var propertyService = this.ServiceProvider.MapToPersitableService(type);
 
             Thing containerInfo = null;
+
             if (!metaInfo.IsTopContainer)
             {
                 containerInfo = this.GetContainerInfo(compositeThing).Thing;
             }
 
-            var persistedThing = compositeThing;
+            var persistedThing = resolvedInfo.Thing ?? compositeThing;
+
             if (persistedThing == null)
             {
                 Logger.Info("The item '{0}' with iid: '{1}' was already deleted: continue processing.", type, deletedValueId);
@@ -1328,7 +1350,6 @@ namespace CDP4WebServices.API.Services.Operations
             // keep a copy of the orginal thing to pass to the after delete hook
             var originalThing = persistedThing.DeepClone<Thing>();
             var securityContext = new RequestSecurityContext { ContainerReadAllowed = true };
-
 
             // call before delete hook
             this.OperationSideEffectProcessor.BeforeDelete(persistedThing, containerInfo, transaction, resolvedInfo.Partition, securityContext);
