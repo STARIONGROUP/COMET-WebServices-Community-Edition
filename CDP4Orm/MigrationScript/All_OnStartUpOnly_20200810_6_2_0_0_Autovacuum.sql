5d26f2ee34ac141fbe627b249bffaab0a35252db
a.vorobiev <a.vorobiev@rheagroup.com>
2020-08-16 20:53:20 +0200

    Autovacuum migration now inline with migration procedure and is doing way less useless stuff.
    Server version in logs.

--- CDP4Orm/MigrationScript/All_OnStartUpOnly_20200810_6_2_0_0_Autovacuum.sql
+++ CDP4Orm/MigrationScript/All_OnStartUpOnly_20200810_6_2_0_0_Autovacuum.sql
@@ -1,13 +1,13 @@
 ﻿DO $$DECLARE r record;
 BEGIN
     FOR r IN 
-        (SELECT 'ALTER TABLE "'|| schemaname || '"."' || tablename ||'" SET (autovacuum_vacuum_scale_factor = 0.0);' as a FROM pg_tables WHERE NOT schemaname IN ('pg_catalog', 'information_schema') ORDER BY schemaname, tablename)
+        (SELECT 'ALTER TABLE "SchemaName_Replace"."' || tablename ||'" SET (autovacuum_vacuum_scale_factor = 0.0);' as a FROM pg_tables WHERE schemaname='SchemaName_Replace' ORDER BY tablename)
 		union all
-		(SELECT 'ALTER TABLE "'|| schemaname || '"."' || tablename ||'" SET (autovacuum_vacuum_threshold = 2500);' as a FROM pg_tables WHERE NOT schemaname IN ('pg_catalog', 'information_schema') ORDER BY schemaname, tablename)
+		(SELECT 'ALTER TABLE "SchemaName_Replace"."' || tablename ||'" SET (autovacuum_vacuum_threshold = 2500);' as a FROM pg_tables WHERE schemaname='SchemaName_Replace' ORDER BY tablename)
 		union all
-		(SELECT 'ALTER TABLE "'|| schemaname || '"."' || tablename ||'" SET (autovacuum_analyze_scale_factor = 0.0);' as a FROM pg_tables WHERE NOT schemaname IN ('pg_catalog', 'information_schema') ORDER BY schemaname, tablename)
+		(SELECT 'ALTER TABLE "SchemaName_Replace"."' || tablename ||'" SET (autovacuum_analyze_scale_factor = 0.0);' as a FROM pg_tables WHERE schemaname='SchemaName_Replace' ORDER BY tablename)
 		union all
-		(SELECT 'ALTER TABLE "'|| schemaname || '"."' || tablename ||'" SET (autovacuum_analyze_threshold = 2500);' as a FROM pg_tables WHERE NOT schemaname IN ('pg_catalog', 'information_schema') ORDER BY schemaname, tablename)
+		(SELECT 'ALTER TABLE "SchemaName_Replace"."' || tablename ||'" SET (autovacuum_analyze_threshold = 2500);' as a FROM pg_tables WHERE schemaname='SchemaName_Replace' ORDER BY tablename)
 		LOOP
         EXECUTE r.a;
     END LOOP;
