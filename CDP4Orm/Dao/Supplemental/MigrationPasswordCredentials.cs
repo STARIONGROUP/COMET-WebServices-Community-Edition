2e8058594dad2c00b71fce32ecbc5f9b8058e622
Adrian Chivu <adrian.chivu@enea.com>
2020-10-23 14:16:43 +0300

    Add additional product service implementation and additional migration data class.

---
+++ CDP4Orm/Dao/Supplemental/MigrationPasswordCredentials.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MigrationPasswordCredentials.cs" company="RHEA System S.A.">
+//   Copyright (c) 2017-2020 RHEA System S.A.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4Orm.Dao
+{
+    using System;
+
+    /// <summary>
+    /// This class is used only in the migration process updating target user password with source user password
+    /// </summary>
+    public class MigrationPasswordCredentials
+    {
+
+        /// <summary>
+        /// Gets or sets the user identifier
+        /// </summary>
+        public Guid Iid { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the user password
+        /// </summary>
+        public string Password { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the salt
+        /// </summary>
+        public string Salt { get; private set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="iid"></param>
+        /// <param name="password"></param>
+        /// <param name="salt"></param>
+        public MigrationPasswordCredentials(Guid iid, string password, string salt)
+        {
+            this.Iid = iid;
+            this.Password = password;
+            this.Salt = salt;
+        }
+    }
+}
---
+++ CDP4WebServices.API/Services/Supplemental/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿namespace CDP4WebServices.API.Services
+{
+    using CDP4Common.DTO;
+    using CDP4Orm.Dao;
+    using Npgsql;
+
+    /// <summary>
+    /// The Person Service Interface which uses the ORM layer to interact with the data model.
+    /// </summary>
+    partial interface IPersonService
+    {
+        bool UpdateCredentials(NpgsqlTransaction transaction, string partition, Thing thing, MigrationPasswordCredentials credentials);
+    }
+}
