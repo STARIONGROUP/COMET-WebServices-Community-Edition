9dc4c815d517e25b67868e57b65c260e81be0980
Adrian Chivu <adrian.chivu@enea.com>
2020-11-03 11:52:19 +0200

    Remove namespace sort issue.

--- CDP4Orm/Dao/Supplemental/PersonDao.cs
+++ CDP4Orm/Dao/Supplemental/PersonDao.cs
@@ -6,13 +6,18 @@
 
 namespace CDP4Orm.Dao
 {
-    using CDP4Authentication;
-    using CDP4Common.DTO;
-    using Npgsql;
-    using NpgsqlTypes;
     using System;
     using System.Collections.Generic;
     using System.Linq;
+
+    using CDP4Authentication;
+
+    using CDP4Common.DTO;
+
+    using Npgsql;
+
+    using NpgsqlTypes;
+
 
     /// <summary>
     /// The Person Data Access Object which acts as an ORM layer to the SQL database.
