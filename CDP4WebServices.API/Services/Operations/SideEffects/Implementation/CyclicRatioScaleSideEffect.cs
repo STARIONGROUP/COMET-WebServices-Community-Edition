a34e04ee96dd57c999224a86d289d1c73957ae9f
Cozmin Velciu <cozmin.velciu@enea.com>
2021-01-26 17:06:09 +0200

    Add concrete SideEffects for MeasurementScale subclasses
    :(

---
+++ CDP4WebServices.API/Services/Operations/SideEffects/Implementation/CyclicRatioScaleSideEffect.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MeasurementScaleSideEffect.cs" company="RHEA System S.A.">
+//   Copyright (c) 2016-2020 RHEA System S.A.
+//
+//    Author: Cozmin Velciu.
+//
+//    This file is part of CDP4 Web Services Community Edition. 
+//    The CDP4 Web Services Community Edition is the RHEA implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+//    This is an auto-generated class. Any manual changes to this file will be overwritten!
+//
+//    The CDP4 Web Services Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    The CDP4 Web Services Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program. If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4WebServices.API.Services.Operations.SideEffects
+{
+    using CDP4Common.DTO;
+
+    /// <summary>
+    /// The purpose of the <see cref="CyclicRatioScaleSideEffect"/> class is to execute additional logic before and
+    /// after a specific operation is performed.
+    /// </summary>
+    public class CyclicRatioScaleSideEffect : MeasurementScaleSideEffect<CyclicRatioScale>
+    {
+    }
+}
