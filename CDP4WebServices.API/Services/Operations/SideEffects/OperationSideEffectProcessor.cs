4fb92ec2e3706fb17fb04f8249c9b31b673aadb6
a.vorobiev <a.vorobiev@rheagroup.com>
2021-02-04 11:50:28 +0100

    Fix tests

--- CDP4WebServices.API/Services/Operations/SideEffects/OperationSideEffectProcessor.cs
+++ CDP4WebServices.API/Services/Operations/SideEffects/OperationSideEffectProcessor.cs
@@ -1,6 +1,26 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="OperationSideEffectProcessor.cs" company="RHEA System S.A.">
-//   Copyright (c) 2016 RHEA System S.A.
+//    Copyright (c) 2015-2021 RHEA System S.A.
+// 
+//    Author: Sam Gerené, Merlin Bieze, Alex Vorobiev, Naron Phou, Alexander van Delft, Nathanael Smiechowski,
+//                 Ahmed Abulwafa Ahmed
+// 
+//    This file is part of CDP4 Web Services Community Edition.
+//    The CDP4 Web Services Community Edition is the RHEA implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+//    This is an auto-generated class. Any manual changes to this file will be overwritten!
+// 
+//    The CDP4 Web Services Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+// 
+//    The CDP4 Web Services Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+// 
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -12,19 +32,21 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
 
     using CDP4Common;
     using CDP4Common.DTO;
-    using CDP4Common.MetaInfo;
 
     using CDP4WebServices.API.Services.Authorization;
+
     using NLog;
+
     using Npgsql;
 
     /// <summary>
-    /// An operation side effect processor class that executes additional logic before and after operations on a <see cref="Thing"/> instance.
+    /// An operation side effect processor class that executes additional logic before and after operations on a
+    /// <see cref="Thing" /> instance.
     /// </summary>
     public class OperationSideEffectProcessor : IOperationSideEffectProcessor
     {
         /// <summary>
-        /// A <see cref="NLog.Logger"/> instance
+        /// A <see cref="NLog.Logger" /> instance
         /// </summary>
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
@@ -34,12 +56,7 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
         private readonly Dictionary<string, IOperationSideEffect> operationSideEffectMap = new Dictionary<string, IOperationSideEffect>();
 
         /// <summary>
-        /// Gets or sets the <see cref="IRequestUtils"/>.
-        /// </summary>
-        public IRequestUtils RequestUtils { get; set; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="OperationSideEffectProcessor"/> class. 
+        /// Initializes a new instance of the <see cref="OperationSideEffectProcessor" /> class.
         /// </summary>
         /// <param name="operationSideEffects">
         /// The injected operation side effects.
@@ -68,94 +85,15 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
         }
 
         /// <summary>
-        /// The get operation side effect.
+        /// Gets or sets the <see cref="IRequestUtils" />.
         /// </summary>
-        /// <param name="typeName">
-        /// The type name of the <see cref="Thing"/> that is being operated upon.
-        /// </param>
-        /// <returns>
-        /// The <see cref="IOperationSideEffect"/>.
-        /// </returns>
-        public IOperationSideEffect GetOperationSideEffect(string typeName)
-        {
-            return this.operationSideEffectMap[typeName];
-        }
-
-        /// <summary>
-        /// The get operation side effect.
-        /// </summary>
-        /// <param name="thing">
-        /// The <see cref="Thing"/> that is being operated upon.
-        /// </param>
-        /// <returns>
-        /// The <see cref="IOperationSideEffect"/>.
-        /// </returns>
-        public IOperationSideEffect GetOperationSideEffect(Thing thing)
-        {
-            return this.operationSideEffectMap[GetTypeName(thing)];
-        }
-
-        /// <summary>
-        /// The is side effect registered.
-        /// </summary>
-        /// <param name="typeName">
-        /// The type name of the <see cref="Thing"/> that is being operated upon.
-        /// </param>
-        /// <returns>
-        /// The <see cref="bool"/>.
-        /// </returns>
-        public bool IsSideEffectRegistered(string typeName)
-        {
-            return this.operationSideEffectMap.ContainsKey(typeName);
-        }
-
-        /// <summary>
-        /// The is side effect registered.
-        /// </summary>
-        /// <param name="thing">
-        /// The <see cref="Thing"/> that is being operated upon.
-        /// </param>
-        /// <returns>
-        /// The <see cref="bool"/>.
-        /// </returns>
-        public bool IsSideEffectRegistered(Thing thing)
-        {
-            return this.operationSideEffectMap.ContainsKey(GetTypeName(thing));
-        }
-
-        /// <summary>
-        /// Gets all applicable side effects for the given <paramref name="thing"/> by going up the inheritance chain
-        /// and returning the associated <see cref="IOperationSideEffect"/> for each class that has defined one.
-        /// Note that this list includes the <see cref="IOperationSideEffect"/> for the given <paramref name="thing"/>
-        /// as well.
-        /// </summary>
-        /// <param name="thing">
-        /// The given <see cref="Thing"/>.
-        /// </param>
-        /// <returns>
-        /// An <see cref="IEnumerable{T}"/> of all applicable side effects.
-        /// </returns>
-        private IEnumerable<IOperationSideEffect> SideEffects(Thing thing)
-        {
-            var type = GetTypeName(thing);
-
-            do
-            {
-                if (this.IsSideEffectRegistered(type))
-                {
-                    yield return this.GetOperationSideEffect(type);
-                }
-
-                var metaInfo = this.RequestUtils.MetaInfoProvider.GetMetaInfo(type);
-                type = metaInfo.BaseType;
-            } while (!string.IsNullOrEmpty(type));
-        }
+        public IRequestUtils RequestUtils { get; set; }
 
         /// <summary>
         /// Predicate to determine if a property will be validated.
         /// </summary>
         /// <param name="thing">
-        /// The <see cref="Thing"/> for which to check if the property validation is to be included.
+        /// The <see cref="Thing" /> for which to check if the property validation is to be included.
         /// </param>
         /// <param name="propertyName">
         /// The property name.
@@ -172,10 +110,10 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
         /// Execute additional logic before a create operation.
         /// </summary>
         /// <param name="thing">
-        /// The <see cref="Thing"/> instance that will be inspected.
+        /// The <see cref="Thing" /> instance that will be inspected.
         /// </param>
         /// <param name="container">
-        /// The container instance of the <see cref="Thing"/> that is inspected.
+        /// The container instance of the <see cref="Thing" /> that is inspected.
         /// </param>
         /// <param name="transaction">
         /// The current transaction to the database.
@@ -198,10 +136,10 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
         /// Execute additional logic after a successful create operation.
         /// </summary>
         /// <param name="thing">
-        /// The <see cref="Thing"/> instance that will be inspected.
+        /// The <see cref="Thing" /> instance that will be inspected.
         /// </param>
         /// <param name="container">
-        /// The container instance of the <see cref="Thing"/> that is inspected.
+        /// The container instance of the <see cref="Thing" /> that is inspected.
         /// </param>
         /// <param name="originalThing">
         /// The original Thing.
@@ -227,10 +165,10 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
         /// Execute additional logic before an update operation.
         /// </summary>
         /// <param name="thing">
-        /// The <see cref="Thing"/> instance that will be inspected.
+        /// The <see cref="Thing" /> instance that will be inspected.
         /// </param>
         /// <param name="container">
-        /// The container instance of the <see cref="Thing"/> that is inspected.
+        /// The container instance of the <see cref="Thing" /> that is inspected.
         /// </param>
         /// <param name="transaction">
         /// The current transaction to the database.
@@ -243,8 +181,9 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
         /// </param>
         /// <param name="rawUpdateInfo">
         /// The raw update info that was serialized from the user posted request.
-        /// The <see cref="ClasslessDTO"/> instance only contains values for properties that are to be updated.
-        /// It is important to note that this variable is not to be changed likely as it can/will change the operation processor outcome.
+        /// The <see cref="ClasslessDTO" /> instance only contains values for properties that are to be updated.
+        /// It is important to note that this variable is not to be changed likely as it can/will change the operation processor
+        /// outcome.
         /// </param>
         public void BeforeUpdate(Thing thing, Thing container, NpgsqlTransaction transaction, string partition, ISecurityContext securityContext, ClasslessDTO rawUpdateInfo)
         {
@@ -258,10 +197,10 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
         /// Execute additional logic after a successful update operation.
         /// </summary>
         /// <param name="thing">
-        /// The <see cref="Thing"/> instance that will be inspected.
+        /// The <see cref="Thing" /> instance that will be inspected.
         /// </param>
         /// <param name="container">
-        /// The container instance of the <see cref="Thing"/> that is inspected.
+        /// The container instance of the <see cref="Thing" /> that is inspected.
         /// </param>
         /// <param name="originalThing">
         /// The original Thing.
@@ -287,10 +226,10 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
         /// Execute additional logic before a delete operation.
         /// </summary>
         /// <param name="thing">
-        /// The <see cref="Thing"/> instance that will be inspected.
+        /// The <see cref="Thing" /> instance that will be inspected.
         /// </param>
         /// <param name="container">
-        /// The container instance of the <see cref="Thing"/> that is inspected.
+        /// The container instance of the <see cref="Thing" /> that is inspected.
         /// </param>
         /// <param name="transaction">
         /// The current transaction to the database.
@@ -313,10 +252,10 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
         /// Execute additional logic after a successful delete operation.
         /// </summary>
         /// <param name="thing">
-        /// The <see cref="Thing"/> instance that will be inspected.
+        /// The <see cref="Thing" /> instance that will be inspected.
         /// </param>
         /// <param name="container">
-        /// The container instance of the <see cref="Thing"/> that is inspected.
+        /// The container instance of the <see cref="Thing" /> that is inspected.
         /// </param>
         /// <param name="originalThing">
         /// The original Thing.
@@ -339,13 +278,97 @@ namespace CDP4WebServices.API.Services.Operations.SideEffects
         }
 
         /// <summary>
-        /// Utility method to get the type name of a <see cref="Thing"/>.
+        /// The get operation side effect.
+        /// </summary>
+        /// <param name="typeName">
+        /// The type name of the <see cref="Thing" /> that is being operated upon.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IOperationSideEffect" />.
+        /// </returns>
+        public IOperationSideEffect GetOperationSideEffect(string typeName)
+        {
+            return this.operationSideEffectMap[typeName];
+        }
+
+        /// <summary>
+        /// The get operation side effect.
+        /// </summary>
+        /// <param name="thing">
+        /// The <see cref="Thing" /> that is being operated upon.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IOperationSideEffect" />.
+        /// </returns>
+        public IOperationSideEffect GetOperationSideEffect(Thing thing)
+        {
+            return this.operationSideEffectMap[GetTypeName(thing)];
+        }
+
+        /// <summary>
+        /// The is side effect registered.
+        /// </summary>
+        /// <param name="typeName">
+        /// The type name of the <see cref="Thing" /> that is being operated upon.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool" />.
+        /// </returns>
+        public bool IsSideEffectRegistered(string typeName)
+        {
+            return this.operationSideEffectMap.ContainsKey(typeName);
+        }
+
+        /// <summary>
+        /// The is side effect registered.
+        /// </summary>
+        /// <param name="thing">
+        /// The <see cref="Thing" /> that is being operated upon.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool" />.
+        /// </returns>
+        public bool IsSideEffectRegistered(Thing thing)
+        {
+            return this.operationSideEffectMap.ContainsKey(GetTypeName(thing));
+        }
+
+        /// <summary>
+        /// Gets all applicable side effects for the given <paramref name="thing" /> by going up the inheritance chain
+        /// and returning the associated <see cref="IOperationSideEffect" /> for each class that has defined one.
+        /// Note that this list includes the <see cref="IOperationSideEffect" /> for the given <paramref name="thing" />
+        /// as well.
+        /// </summary>
+        /// <param name="thing">
+        /// The given <see cref="Thing" />.
+        /// </param>
+        /// <returns>
+        /// An <see cref="IEnumerable{T}" /> of all applicable side effects.
+        /// </returns>
+        private IEnumerable<IOperationSideEffect> SideEffects(Thing thing)
+        {
+            var type = GetTypeName(thing);
+
+            do
+            {
+                if (this.IsSideEffectRegistered(type))
+                {
+                    yield return this.GetOperationSideEffect(type);
+                }
+
+                var metaInfo = this.RequestUtils.MetaInfoProvider.GetMetaInfo(type);
+                type = metaInfo.BaseType;
+            } while (!string.IsNullOrEmpty(type));
+        }
+
+        /// <summary>
+        /// Utility method to get the type name of a <see cref="Thing" />.
         /// </summary>
         /// <param name="thing">
         /// The thing for which to return the type name.
         /// </param>
         /// <returns>
-        /// The type name of a <see cref="Thing"/>.
+        /// The type name of a <see cref="Thing" />.
         /// </returns>
         private static string GetTypeName(Thing thing)
         {
