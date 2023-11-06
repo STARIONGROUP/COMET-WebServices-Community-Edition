738c7b49020aabeb08c475460def488b6b272296
a.vandelft@rheagroup.com <a.vandelft@rheagroup.com>
2023-11-06 10:41:31 +0100

    CodeGen new headers

--- CometServer/AutoGenServices/IEngineeringModelSetupService.cs
+++ CometServer/AutoGenServices/IEngineeringModelSetupService.cs
@@ -1,22 +1,23 @@
 // --------------------------------------------------------------------------------------------------------------------
 // <copyright file="IEngineeringModelSetupService.cs" company="RHEA System S.A.">
-//    Copyright (c) 2015-2021 RHEA System S.A.
+//    Copyright (c) 2015-2023 RHEA System S.A.
 //
-//    Author: Sam Geren�, Merlin Bieze, Alex Vorobiev, Naron Phou, Alexander van Delft, Nathanael Smiechowski
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, 
+//            Antoine Théate, Omar Elebiary, Jaime Bernar
 //
-//    This file is part of COMET Web Services Community Edition. 
-//    The COMET Web Services Community Edition is the RHEA implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+//    This file is part of CDP4-COMET Web Services Community Edition. 
+//    The CDP4-COMET Web Services Community Edition is the RHEA implementation of ECSS-E-TM-10-25 Annex A and Annex C.
 //    This is an auto-generated class. Any manual changes to this file will be overwritten!
 //
-//    The COMET Web Services Community Edition is free software; you can redistribute it and/or
+//    The CDP4-COMET Web Services Community Edition is free software; you can redistribute it and/or
 //    modify it under the terms of the GNU Affero General Public
 //    License as published by the Free Software Foundation; either
 //    version 3 of the License, or (at your option) any later version.
 //
-//    The COMET Web Services Community Edition is distributed in the hope that it will be useful,
+//    The CDP4-COMET Web Services Community Edition is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//    Lesser General Public License for more details.
+//    Affero General Public License for more details.
 //
 //    You should have received a copy of the GNU Affero General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
