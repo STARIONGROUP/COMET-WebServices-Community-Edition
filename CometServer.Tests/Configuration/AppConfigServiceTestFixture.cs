0e7dc6303637a2b2a812e4673e0e72adc5a54482
samatrhea <s.gerene@rheagroup.com>
2023-11-19 19:12:03 +0100

    [Improve] code coverage for AppConfigService

---
+++ CometServer.Tests/Configuration/AppConfigServiceTestFixture.cs
@@ -0,0 +1,72 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppConfigServiceTestFixture.cs" company="RHEA System S.A.">
+//    Copyright (c) 2015-2023 RHEA System S.A.
+// 
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Antoine Théate
+// 
+//    This file is part of CDP4-COMET Webservices Community Edition.
+//    The CDP4-COMET Web Services Community Edition is the RHEA implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+//    This is an auto-generated class. Any manual changes to this file will be overwritten!
+// 
+//    The CDP4-COMET Web Services Community Edition is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Affero General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+// 
+//    The CDP4-COMET Web Services Community Edition is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+// 
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CometServer.Tests.Configuration
+{
+    using System.IO;
+    using CometServer.Configuration;
+    using Microsoft.Extensions.Configuration;
+
+    using NUnit.Framework;
+    
+    /// <summary>
+    /// Suite of tests for the <see cref="AppConfigService"/>
+    /// </summary>
+    [TestFixture]
+    public class AppConfigServiceTestFixture
+    {
+        private IConfiguration configuration;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "appsettings.json");
+
+            // Initialize the configuration builder
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(path, optional: false, reloadOnChange: true);
+
+            // Build the IConfiguration instance
+            this.configuration = configurationBuilder.Build();
+        }
+
+        [Test]
+        public void Verify_that_configuration_is_loaded_from_appsettings()
+        {
+            var appConfigService = new AppConfigService(this.configuration);
+
+            Assert.That(appConfigService.AppConfig.Backtier.Database, Is.EqualTo("cdp4server"));
+
+            Assert.That(appConfigService.AppConfig.Changelog.CollectChanges, Is.True);
+
+            Assert.That(appConfigService.AppConfig.EmailService.SMTP, Is.EqualTo("smtp.cdp4.org"));
+
+            Assert.That(appConfigService.AppConfig.HealthConfig.RequireHost, Is.Empty);
+
+            Assert.That(appConfigService.AppConfig.Midtier.BacktierWaitTime, Is.EqualTo(333));
+        }
+    }
+}
